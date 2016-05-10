634dbc59f213a1ac60be3627c5706a3e079193dd
Willyb15 <william.steen.bryant@gmail.com>
2016-05-10 18:04:48 -0400

    aws

--- front-end/js/main.js
+++ front-end/js/main.js
@@ -1,5 +1,5 @@
 var coffeeApp = angular.module('coffeeApp', ['ngRoute', 'ngCookies']);
-var apiUrl = 'http://willybman.com:3000';
+var apiUrl = 'http://54.193.72.70:3000';
 
 coffeeApp.config(function($routeProvider) {
     $routeProvider.when('/', {
